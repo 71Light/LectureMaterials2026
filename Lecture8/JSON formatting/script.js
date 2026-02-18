8cb4c57fde4b4abd3924c1709f57313169cbccf6
Tom Glover <mrethol@gmail.com>
2026-02-18 10:36:34 +0100

    Example of function lost in stringify

---
+++ Lecture8/JSON formatting/script.js
@@ -0,0 +1,12 @@
+// Create an object with a simple function
+const example = {
+    name: "Test Object",
+    sayHello: function () {
+        return "Hello!";
+    }
+};
+
+// Convert to JSON
+const jsonString = JSON.stringify(example);
+
+console.log(jsonString);
